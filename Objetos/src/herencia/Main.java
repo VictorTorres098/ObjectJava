d8a0b970d24a787174c45c11cec386802ae73e30
victortorres <victortorres098@gmail.com>
2021-10-06 15:35:02 -0300

    actualizado especificacion

--- Objetos/src/herencia/Main.java
+++ Objetos/src/herencia/Main.java
@@ -1,9 +1,19 @@
 package herencia;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Main {
 
 	public static void main(String[] args) {
 		MesaComun Mesacomun = new MesaComun(null, null, null);
+		MesaComun mesa2 = new MesaComun(null,null,null);
+		
+		ArrayList listaMesas = new ArrayList <>();
+		listaMesas.add(Mesacomun);
+		HashMap <Integer, ArrayList> franjaHoraria = new HashMap<>();
+		franjaHoraria.put(1, listaMesas);
+		franjaHoraria.get(1).add(mesa2);
 
 	}
 
